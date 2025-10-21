44f1e1de822574dda5a9af8a3942f457a1fec8db
vedantparasharr <iemvedant@gmail.com>
2025-10-21 22:59:26 +0530

    cleaned html, added click feature in js using query selector

--- script.js
+++ script.js
@@ -1,43 +1,49 @@
+//load and render calculation
 let calculation = localStorage.getItem('calculation') || '';
 display();
 
+//display function
 function display() {
     document.querySelector('.display-calculate').innerHTML = `${calculation}`;
 }
 
+// update calculation
 function updateCalculation(value) {
     calculation += value;
     display();
     localStorage.setItem('calculation', calculation);
 }
 
-// button on press
+// button on click
+document.querySelectorAll('.btn-number, .btn-operator').forEach((button) => {
+    const value = button.dataset.value;
+    if (value) {
+        button.addEventListener('click', () => {
+            updateCalculation(value)
+        })
+    }
+});
 
-document.addEventListener("keydown", (event) => {
-    console.log(event.key);
-    if (event.key === `1`) updateCalculation('1');
-    if (event.key === `2`) updateCalculation('2');
-    if (event.key === `3`) updateCalculation('3');
-    if (event.key === `4`) updateCalculation('4');
-    if (event.key === `5`) updateCalculation('5');
-    if (event.key === `6`) updateCalculation('6');
-    if (event.key === `7`) updateCalculation('7');
-    if (event.key === `8`) updateCalculation('8');
-    if (event.key === `9`) updateCalculation('9');
-    if (event.key === `0`) updateCalculation('0');
-    if (event.key === `.`) updateCalculation('.');
-    if (event.key === `=` || event.key === `Enter`) {
+// equals button
+document.getElementById('equals').addEventListener('click', () => {
+    try {
         calculation = eval(calculation);
+    display();
+    localStorage.setItem('calculation', calculation);
+    } catch {
+        calculation = 'Error';
         display();
-        localStorage.setItem('calculation', calculation);
     }
-    if (event.key === `/`) updateCalculation('/');
-    if (event.key === `+`) updateCalculation('+');
-    if (event.key === `-`) updateCalculation('-');
-    if (event.key === `*`) updateCalculation('*');
-    if (event.key === `Backspace`) {
-        calculation = '';
-        display();
-        localStorage.setItem('calculation', calculation);
-    }
+});
+
+//clear button
+document.getElementById('clear').addEventListener('click', () => {
+    calculation = '';
+    display();
+    localStorage.setItem('calculation', calculation);
+})
+
+// keyboard support
+document.addEventListener("keydown", (event) => {
+    
 })
